84b6eb1c51e797e760a1e956832997e2c61cb585
Charlotte Adams <adams@MacBook-van-Charlotte.local>
2020-12-16 21:52:24 +0100

    clean up

--- mztab code/combine_MIST_SAINT.R
+++ mztab code/combine_MIST_SAINT.R
@@ -2,78 +2,60 @@ library(tidyverse)
 library(data.table)
 library(VennDiagram)
 
-#   Gene names
+#   Load gene names
 fasta_headers <- "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Workspace/fasta/fasta_headers.csv"
 unmapped_headers <- fread(fasta_headers, header = FALSE) %>% as_tibble
 gene_names <- unmapped_headers %>% separate(V3, into=c("pre", "gene"), sep = (" GN=")) %>% drop_na(gene) %>%
 	separate(gene, into = c("gene_name", "post", "ppost") ,sep=(" ")) %>%
 	select(V2, gene_name)
 
-#   output MiST
-# Before you can load the MistOutput the first couple of lines must be removed.
+#	Load MiST output
+# 	Before you can load the MistOutput the first couple of lines must be removed.
 MIST <- fread("/Users/adams/Documents/PhD/SARS-CoV-2/Data/Workspace/MiST/MistOutput.txt", sep= "\t") %>% as_tibble
+MIST_output <- merge(MIST, gene_names, by.x = "Prey", by.y = "V2") %>% 		# Add gene names
+	as_tibble %>% mutate(PreyGene = gene_name) %>% mutate(idBait=Bait) %>%
+	filter(!MiST == 0) %>% select(idBait, Prey, everything()) %>%
+	unite(BP, idBait:Prey ,sep ="_", remove=FALSE) %>%
+	select(idBait, PreyGene, everything()) %>% unite(BP_gene, idBait:PreyGene ,sep ="_", remove=FALSE)
 
-MIST_result <- MIST %>% filter(MiST >= 0.7) %>%
-	mutate(idBait=Bait) %>%
-	select(idBait, Prey, everything()) %>%
-	unite(BP, idBait:Prey ,sep ="_", remove=FALSE)
+#	Load SAINT output
+SAINT_output <- fread("/Users/adams/Documents/PhD/SARS-CoV-2/Data/Workspace/SAINT/list.txt", sep= "\t") %>% as_tibble %>%
+	mutate(idBait=Bait) %>% select(idBait, Prey, everything()) %>%
+	unite(BP, idBait:Prey ,sep ="_", remove=FALSE) %>%
+	select(idBait, PreyGene, everything()) %>% unite(BP_gene, idBait:PreyGene ,sep ="_", remove=FALSE)
 
-mist_gene <- merge(MIST, gene_names, by.x = "Prey", by.y = "V2") %>% as_tibble
+#	Combine the MIST and SAINT output
+MIST_filter <- MIST_output %>% filter(BP %in% SAINT_output$BP)
+SAINT_filter <- SAINT_output %>% filter(BP %in% MIST_output$BP)
+SAINT_MIST <- merge(MIST_filter, SAINT_filter) %>% as_tibble
 
-MIST_gene <- mist_gene %>% filter(MiST >= 0.7) %>%
-	mutate(idBait=Bait) %>%
-	mutate(PreyGene = gene_name) %>%
-	select(idBait, PreyGene, everything()) %>%
-	unite(BP, idBait:PreyGene ,sep ="_", remove=FALSE)
+#	Filter the results
+SAINT_MIST_filtered <- SAINT_MIST %>% filter(MiST >= 0.7 & BFDR <= 0.05 & AvgSpec >= 2)
 
-#   output SAINT
-SAINT <- fread("/Users/adams/Documents/PhD/SARS-CoV-2/Data/Workspace/SAINT/list.txt", sep= "\t") %>% as_tibble
-
-SAINT_result <- SAINT %>% filter(BFDR <= 0.05 & AvgSpec >= 2) %>%
-	mutate(idBait=Bait) %>%
-	select(idBait, Prey, everything()) %>%
-	unite(BP, idBait:Prey ,sep ="_", remove=FALSE)
-
-SAINT_gene <- SAINT %>% filter(BFDR <= 0.05 & AvgSpec >= 2) %>%
-	mutate(idBait=Bait) %>%
-	select(idBait, PreyGene, everything()) %>%
-	unite(BP, idBait:PreyGene ,sep ="_", remove=FALSE)
-
-#   combine MIST and SAINT output
-combined_result <- SAINT_result %>% filter(BP %in% MIST_result$BP)      #in both directions same amount
-combined_gene <- SAINT_gene %>% filter(BP %in% MIST_gene$BP)
-combined_gene_mist <- MIST_gene %>% filter(BP %in% SAINT_gene$BP)
-
-output_path <- "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Spreadsheets/HCIP/SAINT<=0.05_MiST>=0.7_AvgSpec>=2.csv"
-fwrite(combined_gene, output_path, append = FALSE, col.names = TRUE) 
-output_path <- "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Spreadsheets/HCIP/SAINT<=0.05_MiST>=0.7_AvgSpec>=2_MistColumns.csv"
-fwrite(combined_gene_mist, output_path, append = FALSE, col.names = TRUE) 
+fwrite(SAINT_MIST, "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Spreadsheets/HCIP/Scoring results.csv", append = FALSE, col.names = TRUE)
+fwrite(SAINT_MIST_filtered, "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Spreadsheets/HCIP/HCIPs.csv", append = FALSE, col.names = TRUE)
 
 ################################################################################################################################################
 
 #   load original Gordon et al. results
-intact <- fread("/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/HCIP/AP-MS/Gordon/intact_gordon.txt", sep= "\t") %>% as_tibble
-intact_map <- fread("/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results//HCIP/AP-MS/Gordon/name_map_sars_cov_2.txt", sep= "\t")
-intact_annot <- merge(intact, intact_map, by.x = "IDinteractorA", by.y = "accession") %>% as_tibble
+Gordon <- fread("/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/HCIP/AP-MS/Gordon/intact_gordon.txt", sep= "\t") %>% as_tibble
+Gordon_map <- fread("/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results//HCIP/AP-MS/Gordon/name_map_sars_cov_2.txt", sep= "\t")
+Gordon_annot <- merge(intact, intact_map, by.x = "IDinteractorA", by.y = "accession") %>% as_tibble
 
 #   remove space in protein14 (bait)
-intact_annot$idBait <- gsub(" ", "", intact_annot$idBait)
+Gordon_annot$idBait <- gsub(" ", "", intact_annot$idBait)
+Gordon_gene <- merge(intact_annot, gene_names, by.x = "IDinteractorB", by.y = "V2") %>% as_tibble
 
-intact_result <- intact_annot %>%
-	select(idBait, IDinteractorB, everything()) %>%
-	unite(BP, idBait:IDinteractorB,sep ="_", remove=FALSE) #%>% select(BP) %>% unique()
-
-intact_G <- merge(intact_annot, gene_names, by.x = "IDinteractorB", by.y = "V2") %>% as_tibble
-intact_gene <- intact_G %>%
-	select(idBait, gene_name, everything()) %>%
-	unite(BP, idBait:gene_name,sep ="_", remove=FALSE)
+Gordon_result <- Gordon_gene %>% mutate(Prey = IDinteractorB) %>% mutate(PreyGene = gene_name) %>%
+	select(idBait, Prey, everything()) %>% unite(BP, idBait:Prey, sep ="_", remove=FALSE) %>%
+	select(idBait, PreyGene, everything()) %>% unite(BP_gene, idBait:PreyGene, sep ="_", remove=FALSE)
 
 #   Create plot with overlap
-ann <- combined_gene %>% filter(!BP %in% intact_gene$BP) %>% select(idBait, BP) %>% mutate(Identification = "ANN-SoLo")
-gordon <- intact_gene %>% filter(!BP %in% combined_gene$BP) %>% select(idBait, BP) %>% mutate(Identification = "Gordon et al.")
-both <- combined_gene %>% filter(BP %in% intact_gene$BP) %>% select(idBait, BP) %>% mutate(Identification = "Overlap")
+ann <- SAINT_MIST_filtered %>% filter(!BP_gene %in% Gordon_result$BP_gene) %>% select(idBait, BP_gene) %>% mutate(Identification = "ANN-SoLo") %>% unique
+gordon <- Gordon_result %>% filter(!BP_gene %in% SAINT_MIST_filtered$BP_gene) %>% select(idBait, BP_gene) %>% mutate(Identification = "Gordon et al.") %>% unique
+both <- SAINT_MIST_filtered %>% filter(BP_gene %in% Gordon_result$BP_gene) %>% select(idBait, BP_gene) %>% mutate(Identification = "Overlap") %>% unique
 
-comb <- rbind(ann, gordon, both)
+comb <- rbind(ann, gordon, both) 
 
 s <- ggplot(comb, aes(idBait, fill = Identification)) +
 	geom_bar(position = "stack") +
@@ -82,95 +64,43 @@ s <- ggplot(comb, aes(idBait, fill = Identification)) +
 	scale_fill_manual(values=c("#071E22", "#849B96", "#377563")) +
 	theme(axis.text.x = element_text(size=10, angle=90))
 
-ggsave("/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Figures/HCIP/HCIP overlap.png", s,width = 24, height = 9, units = "cm")
-
-################################################################################################################################################
-combined_nsp5 <- combined_gene %>% filter(idBait == "nsp5") 
-intact_nsp5 <- intact_gene %>% filter(idBait == "nsp5") %>%
-	rename(PreyGene = gene_name) %>%
-	select(idBait, BP, PreyGene)
-all_nsp5 <- rbind(combined_nsp5, intact_nsp5) %>% unique
-
-combined_nsp5_C145A <- combined_gene %>% filter(idBait == "nsp5_C145A")
-intact_nsp5_C145A <- intact_gene %>% filter(idBait == "nsp5_C145A") %>%
-	rename(PreyGene = gene_name) %>%
-	select(idBait, BP, PreyGene)
-all_nsp5_C145A <- rbind(combined_nsp5_C145A, intact_nsp5_C145A) %>% unique
-
-combined_N <- combined_gene %>% filter(idBait == "N") 
-intact_N <- intact_gene %>% filter(idBait == "N") %>%
-	rename(PreyGene = gene_name) %>%
-	select(idBait, BP, PreyGene)
-all_N <- rbind(combined_N, intact_N) %>% unique
-
-combined_nsp14 <- combined_gene %>% filter(idBait == "nsp14") 
-intact_nsp14 <- intact_gene %>% filter(idBait == "nsp14") %>%
-	rename(PreyGene = gene_name) %>%
-	select(idBait, BP, PreyGene)
-all_nsp14 <- rbind(combined_nsp14,intact_nsp14) %>% unique
-
-output_path <- "/Users/adams/Documents/master/master 2/Stage en thesis/Data/sars_cov_2/HCIP/nsp5.csv"
-fwrite(all_nsp5, output_path, append = FALSE, col.names = TRUE) 
-output_path <- "/Users/adams/Documents/master/master 2/Stage en thesis/Data/sars_cov_2/HCIP/nsp5_C145A.csv"
-fwrite(all_nsp5_C145A, output_path, append = FALSE, col.names = TRUE) 
-output_path <- "/Users/adams/Documents/master/master 2/Stage en thesis/Data/sars_cov_2/HCIP/N.csv"
-fwrite(all_N, output_path, append = FALSE, col.names = TRUE) 
-output_path <- "/Users/adams/Documents/master/master 2/Stage en thesis/Data/sars_cov_2/HCIP/nsp14.csv"
-fwrite(all_nsp14, output_path, append = FALSE, col.names = TRUE) 
-
-combined_gene %>% filter(idBait == "nsp14") 
-both %>% filter(idBait == "nsp14") %>% select(BP)
-gordon %>% filter(idBait == "nsp14") %>% select(BP)
+ggsave("/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Figures/HCIP/Overlap/HCIP overlap.png", s,width = 24, height = 9, units = "cm")
 
 ################################################################################################################################################
 
-#   reproduce
-nrow(both)/nrow(intact_result)
-nrow(both)/nrow(intact_gene)
-
-nrow(combined_result)/nrow(intact_result) -1
-nrow(combined_gene)/nrow(intact_gene) -1
-
-################################################################################################################################################
 #   Write gene names to files, to color the nodes in network
-combined_genes <- combined_gene %>% select(idBait, BP, PreyGene) %>% unique()
+both_gene <- SAINT_MIST_filtered %>% filter(BP_gene %in% Gordon_result$BP_gene) %>% select(PreyGene)
+ann_gene <- SAINT_MIST_filtered %>% filter(!BP_gene %in% Gordon_result$BP_gene) %>% select(PreyGene)
+gordon_gene <- Gordon_result %>% filter(!BP_gene %in% SAINT_MIST_filtered$BP_gene) %>% select(PreyGene)
+bait_gene <- SAINT_MIST_filtered %>% select(idBait) %>% unique()
 
-ann <- combined_genes %>% filter(!BP %in% intact_gene$BP) %>% mutate(identification = "ANN-SoLo") %>% unique()
-gordon <- intact_gene %>% filter(!BP %in% combined_genes$BP) %>% mutate(identification = "Gordon et al.") %>% 
-	mutate(PreyGene = gene_name) %>%
-	select(idBait, BP, PreyGene) %>% unique()
-both <- combined_genes %>% filter(BP %in% intact_gene$BP) %>% mutate(identification = "Overlap") %>% unique()
+ann_gene %>% filter(PreyGene %in% both_gene$PreyGene)
+Gordon_result %>% filter(PreyGene==c("MOGS", "NARS2"))
 
-both_gene <- both %>% select(PreyGene) %>% unique()
-ann_gene <- ann %>% select(PreyGene) %>% unique()
-gordon_gene <- gordon %>% select(PreyGene) %>% unique()
-bait_gene <- combined_genes %>% select(idBait) %>% unique()
-
-output_path <- "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Spreadsheets/Network/both_gene.csv"
-fwrite(both_gene, output_path, append = FALSE, col.names = TRUE)
-output_path <- "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Spreadsheets/Network/ann_gene.csv"
-fwrite(ann_gene, output_path, append = FALSE, col.names = TRUE)
-output_path <- "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Spreadsheets/Network/gordon_gene.csv"
-fwrite(gordon_gene, output_path, append = FALSE, col.names = TRUE)
-output_path <- "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Spreadsheets/Network/baits.csv"
-fwrite(bait_gene, output_path, append = FALSE, col.names = TRUE)
+output_path <- "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Spreadsheets/Network/"
+fwrite(both_gene, paste(output_path, "both_gene.txt", sep=""))
+fwrite(ann_gene, paste(output_path, "ann_gene.txt", sep=""))
+fwrite(gordon_gene, paste(output_path, "gordon_gene.txt", sep=""))
+fwrite(bait_gene, paste(output_path, "bait_gene.txt", sep=""))
 
 #   Save file that contains both the ANN-SoLo as the Gordon et al results to put into a network
-gordon_combined_genes <- rbind(combined_genes, gordon)
+ANNSoLo <- SAINT_MIST_filtered %>% select(BP_gene, idBait, PreyGene)
+Gordonetal <- Gordon_result %>% select(BP_gene, idBait, PreyGene)
+gordon_combined_genes <- rbind(ANNSoLo, Gordonetal) %>% unique
 output_path <- "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Spreadsheets/Network/ann_gordon_gene.csv"
 fwrite(gordon_combined_genes, output_path, append = FALSE, col.names = TRUE)
 
 ################################################################################################################################################
 
 #   Save BP and gene name to create a venn diagram
-gordon_bp <- intact_gene %>% select(BP)
-gordon_gn <- intact_gene %>% select(gene_name) %>% unique
+gordon_bp <- Gordon_result %>% select(BP_gene)
+gordon_gn <- Gordon_result %>% select(PreyGene) %>% unique
 output_path <- "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/HCIP/AP-MS/Gordon/"
 fwrite(gordon_bp, paste(output_path, "gordon_bp.txt", sep=""))
 fwrite(gordon_gn, paste(output_path, "gordon_gn.txt", sep=""))
 
-annsolo_bp <- combined_gene %>% select(BP)
-annsolo_gn <- combined_gene %>% select(PreyGene) %>% unique
+annsolo_bp <- SAINT_MIST_filtered %>% select(BP_gene)
+annsolo_gn <- SAINT_MIST_filtered %>% select(PreyGene) %>% unique
 output_path <- "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/HCIP/AP-MS/ANN-SoLo/"
 fwrite(annsolo_bp, paste(output_path, "annsolo_bp.txt", sep=""))
 fwrite(annsolo_gn, paste(output_path, "annsolo_gn.txt", sep=""))
@@ -212,27 +142,47 @@ output_path <- "/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/HCIP/BioID/Gi
 fwrite(Gingras_bp, paste(output_path, "Gingras_bp.txt", sep=""))
 fwrite(Gingras_gn, paste(output_path, "Gingras_gn.txt", sep=""))
 
-#   Venn Diagram
-# venn.plot <- draw.pairwise.venn(322, 612, 210, category = c("Gordon et al.", "ANN-SoLo"), 
-# 	scaled = TRUE, inverted = TRUE, ext.text = TRUE, ext.percent = rep(0.05, 3),
-# 	lwd = rep(2, 2), lty = rep("solid", 2), col = rep("black",2),
-# 	fill = NULL, alpha = rep(0.5, 2), label.col = rep("black", 3),
-# 	cex = rep(1, 3), fontface = rep("plain", 3), fontfamily = rep("Helvetica", 3),
-# 	cat.pos = c(-50, 50), cat.dist = rep(0.025, 2), cat.cex = rep(1, 2), cat.col = rep("black", 2),
-# 	cat.fontface = rep("plain", 2), cat.fontfamily = rep("Helvetica", 2), cat.just = rep(list(c(0.5, 0.5)), 2),
-# 	cat.default.pos = "outer", cat.prompts = FALSE, ext.pos = rep(0, 2), ext.dist = rep(0, 2),
-# 	ext.line.lty = "solid", ext.length = rep(0.95, 2), ext.line.lwd = 1, rotation.degree = 0,
-# 	rotation.centre = c(0.5, 0.5), ind = TRUE, sep.dist = 0.05, offset = 0, cex.prop = NULL,
-# 	print.mode = "raw", sigdigs = 3)
-# grid.draw(venn.plot);
-# grid.newpage();
-
-venn <- draw.pairwise.venn(322, 612, 210,
+#	Create the venn diagram
+venn <- draw.pairwise.venn(332, 553, 200,
 	category = rep(c("Gordon et al.", "ANN-SoLo")), cex = 2, cat.cex = 2,
 	fontfamily = "Helvetica", cat.fontfamily = "Helvetica",
 	rscaled = TRUE, cat.dist = c(0.04, 0.04),
 	cat.pos = c(-14, 10))
 grid.draw(venn)
 
-ggsave(venn, file="/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/HCIP/double_Venn.pdf", width = 17, height = 17, units = "cm")
+ggsave(venn, file="/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Figures/Venn/Overlap.pdf", width = 17, height = 17, units = "cm")
+
+################################################################################################################################################
+
+#   Load original Gordon et al. results
+Gordon <- fread("/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/HCIP/AP-MS/Gordon/Scoring results.csv", header = TRUE) %>% as_tibble
+Gordon_output <- Gordon %>% separate(Bait, into=c("x", "idBait"), sep=" ") %>%
+	mutate(idBait = str_replace(idBait,"Spike", "S")) %>%
+	mutate(idBait = str_replace(idBait,"orf9c", "orf9c(protein14)")) %>%
+	mutate(BFDR = Saint_BFDR) %>% mutate(MiST = MIST) %>% mutate(Prey = Preys) %>%
+	select(idBait, Prey, everything()) %>% unite(BP, idBait:Prey, sep ="_", remove=FALSE) %>%
+	select(idBait, PreyGene, everything()) %>% unite(BP_gene, idBait:PreyGene, sep ="_", remove=FALSE)
+Gordon_filtered <- Gordon_output %>% filter(MiST >= 0.7 & BFDR <= 0.05 & AvgSpec >= 2)
+
+#	Plot the MiST scoring against the BFDR
+Gordon_input <- Gordon_output %>% mutate(MiST_Gordon = MiST) %>% mutate(BFDR_Gordon = BFDR) %>% select(BP_gene, idBait, PreyGene, Prey, MiST_Gordon, BFDR_Gordon)
+ANNSoLo_input <- SAINT_MIST %>% mutate(MiST_ANNSoLo = MiST) %>% mutate(BFDR_ANNSoLo = BFDR) %>% select(BP_gene, idBait, PreyGene, Prey, MiST_ANNSoLo, BFDR_ANNSoLo)
+Plot_input <- merge(Gordon_input, ANNSoLo_input) %>% as_tibble
+
+s <- ggplot(Plot_input, aes(MiST_Gordon, MiST_ANNSoLo)) +
+	geom_point(alpha = 0.2) +
+	theme_minimal() +
+	labs(title ="MiST scores of Gordon et al. plotted against those of ANN-SoLo", x = "MiST score Gordon et al.", y = "MiST score ANN-SoLo") + 
+	theme(axis.text.x = element_text(size=10, angle=90))
+ggsave(s, file="/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Figures/Explorative plots/MiST scores of Gordon et al. plotted against those of ANN-SoLo.pdf", width = 17, height = 17, units = "cm")
+
+s <- ggplot(Plot_input, aes(BFDR_Gordon, BFDR_ANNSoLo)) +
+	geom_point(alpha = 0.2) +
+	theme_minimal() +
+	labs(title ="SAINT BFDR scores of Gordon et al. plotted against those of ANN-SoLo", x = "BFDR Gordon et al.", y = "BFDR ANN-SoLo") + 
+	theme(axis.text.x = element_text(size=10, angle=90))
+ggsave(s, file="/Users/adams/Documents/PhD/SARS-CoV-2/Data/Results/Figures/Explorative plots/SAINT BFDR scores of Gordon et al. plotted against those of ANN-SoLo.pdf", width = 17, height = 17, units = "cm")
+
+Gordon_output %>% summarise(a = mean(MiST), b = mean(BFDR))
+SAINT_MIST %>% summarise(a = mean(MiST), b = mean(BFDR))
 
