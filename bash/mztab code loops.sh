b1bb5756ffd7b6184259a6527b6c8768bf6c55c7
Charlotte Adams <adams@macbookharlotte.uantwerpen.be>
2022-01-11 09:29:43 +0100

    Plot SSM loop

--- bash/mztab code loops.sh
+++ bash/mztab code loops.sh
@@ -25,3 +25,23 @@ do
     IFS=';' read -r -a array <<< "$line"
     time python3 mztab\ code/mztab_to_csv.py /Users/adams/Documents/PhD/SARS-CoV-2/Data/Workspace/mztab/${array[0]}.mztab "${array[0]}"
 done
+
+#   Plot SSM
+
+name_file=/Users/adams/Documents/PhD/SARS-CoV-2/Data/Workspace/names/SNO.txt
+lines=`tail -n+1 $name_file`
+for line in $lines
+do
+    IFS=';' read -r -a array <<< "$line"
+    time /Users/adams/anaconda3/envs/ann_solo/bin/python3 mztab\ code/plot_ssm.py  "${array[0]}"  "${array[1]}"  "${array[2]}"  "${array[3]}"
+done
+
+#   Plot SSM
+
+name_file=/Users/adams/Documents/PhD/SARS-CoV-2/Data/Workspace/names/UBI.txt
+lines=`tail -n+1 $name_file`
+for line in $lines
+do
+    IFS=';' read -r -a array <<< "$line"
+    time /Users/adams/anaconda3/envs/ann_solo/bin/python3 mztab\ code/plot_ssm.py  "${array[0]}"  "${array[1]}"  "${array[2]}"  "${array[3]}"
+done
