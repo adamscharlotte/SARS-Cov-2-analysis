908e9d5b8c50b3489af101421e6374b4372cb432
adamscharlotte <adams@MacBook-Pro.local>
2022-03-28 15:05:31 +0200

    rename folder

--- CLI code/run_cwl.sh
+++ CLI code/run_cwl.sh
@@ -1,13 +1,11 @@
-#   Directory
-cd Documents/PhD/SARS-CoV-2/Data/Workspace/
-
-#   Proteins
-name_file=names/mztab_names.txt
+# Perform protein inference ------------------------------------------------------------------------------------------------------------------------
+#	
+name_file=filenames.txt
 lines=`tail -n+1 $name_file`
 for line in $lines
 do
     IFS=';' read -r -a array <<< "$line"
-    /Users/adams/anaconda3/bin/cwltool --outdir proteins /Users/adams/Desktop/SARS-CoV-2-analysis/CWL\ workflows/Protein_Inference_workflow.cwl --mztab_to_idxml_py /Users/adams/Desktop/SARS-CoV-2-analysis/mztab\ code/mztab_to_idxml.py --mztab mztab/${array[0]}.mztab --fasta fasta/uniprot-proteome_homo_sapiens_reference_proteome_5_23_2016_sars_cov_2.fasta --idxml_output ${array[0]}.idxml --cp /Users/adams/Downloads/pia-1.3.11/pia-1.3.11.jar --compiler de.mpc.pia.intermediate.compiler.PIACompiler --bait ${array[0]} --xml_output ${array[0]}.xml --jar /Users/adams/Downloads/pia-1.3.11/pia-1.3.11.jar --paramFile param/pia_pipe_SARS_v3.xml --proteinExport ${array[0]}.mztab --mztab_to_csv_py /Users/adams/Desktop/SARS-CoV-2-analysis/mztab\ code/mztab_to_protein_csv.py --pia_proteins_R /Users/adams/Desktop/SARS-CoV-2-analysis/mztab\ code/pia_proteins.R
+    /Users/adams/anaconda3/bin/cwltool --outdir proteins /CWL\ workflows/Protein_Inference_workflow.cwl --mztab_to_idxml_py /mztab\ code/mztab_to_idxml.py --mztab ${array[0]}.mztab --fasta uniprot-proteome_homo_sapiens_reference_proteome_5_23_2016_sars_cov_2.fasta --idxml_output ${array[0]}.idxml --cp pia-1.3.11.jar --compiler de.mpc.pia.intermediate.compiler.PIACompiler --bait ${array[0]} --xml_output ${array[0]}.xml --jar /Users/adams/Downloads/pia-1.3.11/pia-1.3.11.jar --paramFile param/pia_pipe_SARS_v3.xml --proteinExport ${array[0]}.mztab --mztab_to_csv_py /mztab\ code/mztab_to_protein_csv.py --pia_proteins_R /mztab\ code/pia_proteins.R
 done
 
 #   Modifications
--- bash/SAINT.sh
+++
@@ -1,6 +0,0 @@
-cd /Users/adams/Documents/PhD/SARS-CoV-2/Data/Workspace/SAINT
-
-/Users/adams/Downloads/SAINTexpress_v3.6__2014-12-04/bin/SAINTexpress-spc interaction_file.txt prey_file.txt bait_file.txt
-
-# /Users/adams/Downloads/SAINT_v2.4.0/src/SAINTspc-ctrl interaction_file.txt prey_file.txt bait_file.txt
-# /Users/adams/Downloads/SAINT_v2.5-2.0/bin/saint-reformat interaction_file.txt bait_file.txt prey_file.txt
