be20724d70e74ea4e04615e13c685e45ae6062ef
adamscharlotte <adams@macbook-pro.uantwerpen.be>
2022-04-07 15:50:28 +0200

    generalize code

--- CWL-workflows/Code/mztab_to_idxml.py
+++ CWL-workflows/Code/mztab_to_idxml.py
@@ -6,7 +6,7 @@ import argparse, pathlib
 
 parser = argparse.ArgumentParser()
 parser.add_argument('mztab', type=pathlib.Path)
-parser.add_argument('bait', type=str)
+parser.add_argument('idxml', type=pathlib.Path)
 args = parser.parse_args()
 
 # path = "/Users/adams/Documents/master/master 2/Stage en thesis/Data/sars_cov_2/"
@@ -14,7 +14,7 @@ args = parser.parse_args()
 # mztab = path + 'mztab/' + bait + '.mztab'
 # output_path = path + 'peptideIndexer/test/' + bait + '.idxml'
 
-output_path = args.bait + '.idxml'
+output_path = args.idxml
 
 run_name = 'unknown'
 skiplines = 0
