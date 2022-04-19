4cb7a6a0744dadce218df3b3bb097781acaea2b1
adamscharlotte <adams@MacBook-Pro.local>
2022-04-19 15:50:47 +0200

    generalize code

--- Analysis-code/mztab_to_psm_csv.py
+++ Analysis-code/mztab_to_psm_csv.py
@@ -1,28 +1,24 @@
 import sys
-#print(sys.executable)       #sys.executable contains full path of the currently running Python interpreter
 import pandas as pd
 import sys
 import argparse, pathlib
 
 parser = argparse.ArgumentParser()
 parser.add_argument('mztab', type=pathlib.Path)
-parser.add_argument('bait', type=str)
+parser.add_argument('csv', type=pathlib.Path)
 args = parser.parse_args()
 
-# path = '/Users/adams/Documents/PhD/SARS-CoV-2/Data/Workspace/'
-# output_path = path + 'csv/psm/' + args.bait + '.csv'
-output_path = args.bait + '.csv'
+output_path = args.csv
 
 with open (args.mztab,'r') as f:
-    outF = open(output_path, 'w')
-    for line in f:
-        if line.startswith('MTD'):
-            continue
-        if line.startswith('PRT'):
-            continue
-        if line.startswith('PRH'):
-            continue
-        else:
-            outF.write(line)
-    outF.close()
-            #mztabList.append(line)
+	outF = open(output_path, 'w')
+	for line in f:
+		if line.startswith('MTD'):
+			continue
+		if line.startswith('PRT'):
+			continue
+		if line.startswith('PRH'):
+			continue
+		else:
+			outF.write(line)
+	outF.close()
--- CWL-workflows/Code/mztab_to_csv.py
+++ CWL-workflows/Code/mztab_to_csv.py
@@ -6,22 +6,19 @@ import argparse, pathlib
 
 parser = argparse.ArgumentParser()
 parser.add_argument('mztab', type=pathlib.Path)
-parser.add_argument('bait', type=str)
+parser.add_argument('csv', type=pathlib.Path)
 args = parser.parse_args()
 
 path = '/Users/adams/Documents/PhD/SARS-CoV-2/Data/Workspace/'
-# output_path = path + 'csv/mztab/' + args.bait + '.csv'
-output_path = args.bait + '.csv'
+output_path = args.csv
 
 with open (args.mztab,'r') as f:
-    outF = open(output_path, 'w')
-    for line in f:
-        if line.startswith('MTD'):
-            #mztabHead.append(line)
-            continue
-        else:
-            outF.write(line)
-    outF.close()
-            #mztabList.append(line)
-
-#data = pd.read_csv(output_path, sep = '\t')
+	outF = open(output_path, 'w')
+	for line in f:
+		if line.startswith('MTD'):
+			#mztabHead.append(line)
+			continue
+		else:
+			outF.write(line)
+	outF.close()
+			#mztabList.append(line)
