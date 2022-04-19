321ac0e8ab892a25ca4b81166161798af27c2898
adamscharlotte <adams@MacBook-Pro.local>
2022-04-19 15:47:49 +0200

    generalize code

--- CWL-workflows/Code/mztab_to_idxml.py
+++ CWL-workflows/Code/mztab_to_idxml.py
@@ -9,11 +9,6 @@ parser.add_argument('mztab', type=pathlib.Path)
 parser.add_argument('idxml', type=pathlib.Path)
 args = parser.parse_args()
 
-# path = "/Users/adams/Documents/master/master 2/Stage en thesis/Data/sars_cov_2/"
-# bait = "qx017077"
-# mztab = path + 'mztab/' + bait + '.mztab'
-# output_path = path + 'peptideIndexer/test/' + bait + '.idxml'
-
 output_path = args.idxml
 
 run_name = 'unknown'
@@ -26,20 +21,8 @@ with open(args.mztab) as f_in:
         line = next(f_in)
         skiplines += 1
 
-# run_name = 'unknown'
-# skiplines = 0
-# with open(mztab) as f_in:
-#     line = next(f_in)
-#     while line.split('\t', 1)[0] != 'PSH':
-#         if 'ms_run[1]-location' in line:
-#             run_name = line.split('\t')[2]
-#         line = next(f_in)
-#         skiplines += 1
-
 psms = pd.read_csv(args.mztab, sep='\t', header=skiplines, index_col='PSM_ID')
-# psms = pd.read_csv(args.mztab, sep='\t', header=skiplines)
 
-# psms = pd.read_csv(mztab, sep='\t', header=skiplines, index_col='PSM_ID')
 peptide_ids = []
 for _, psm in psms.iterrows():
     peptide_id = pyms.PeptideIdentification()
